2ca137b036125a374ecbe04424d7b193b98f4b41
Dan Kingo <danieldejen23@gmail.com>
2024-04-03 23:30:27 +0300

    finish footer section

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 document.getElementById("show").onclick = function () {
   document.querySelector(".sidebar").style.display = "flex";
 };
-document.getElementById("hide").onclick = function () {
+document.getElementById("hide").onclick = function (e) {
   document.querySelector(".sidebar").style.display = "none";
+  e.preventDefault();
 };
 
 let loader = document.querySelector(".loader-container");
