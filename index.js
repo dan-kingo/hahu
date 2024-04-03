73f1e0aa1f8e35e4c441c5c15f9e400d9f14a761
Dan Kingo <danieldejen23@gmail.com>
2024-04-03 21:11:15 +0300

    finish sidbar

---
+++ index.js
@@ -0,0 +1,6 @@
+document.getElementById("show").onclick = function () {
+  document.querySelector(".sidebar").style.display = "flex";
+};
+document.getElementById("hide").onclick = function () {
+  document.querySelector(".sidebar").style.display = "none";
+};
